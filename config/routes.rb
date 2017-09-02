3597e00133397a969d7fd4ba69d4672d25329d89
doberman7 <ivanrubioangeles@gmail.com>
2017-09-01 21:10:38 -0500

    reinicio generacion de controladores, inclución de recursos

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'tag/index'
-
-  get 'message/index'
-
-  root 'message#index'
-
-  resources :messages
-  resources :tags
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/tag_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TagControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tag_index_url
-    assert_response :success
-  end
-
-end
